2411e26e0c6ae20d32ca0b0c612920af47f87214
Rimantas <belauzas@yahoo.com>
2021-05-24 20:20:51 +0300

    Falsy sum

--- js/sum/sum.test.js
+++ js/sum/sum.test.js
@@ -15,3 +15,31 @@ test('adds 0.5 + 0.5 to equal 1', () => {
 test('adds 0.1 + 0.2 to equal 0.3', () => {
     expect(sum(0.1, 0.2)).toBeCloseTo(0.3, 5);
 });
+
+test('adds 13, 8 to equal 21', () => {
+    expect(sum(13, 8)).toBe(21);
+});
+
+test('string is not allowed', () => {
+    expect(sum('a', 8)).toBeFalsy();
+});
+
+test('string is not allowed', () => {
+    expect(sum(NaN, 'a')).toBeFalsy();
+});
+
+test('string is not allowed', () => {
+    expect(sum('a', NaN)).toBeFalsy();
+});
+
+test('string is not allowed', () => {
+    expect(sum(NaN, 8)).toBe(false);
+});
+
+test('string is not allowed', () => {
+    expect(sum(8, NaN)).toBe(false);
+});
+
+test('string is not allowed', () => {
+    expect(sum(8, 'a')).toBeFalsy();
+});
