37ed037c7ecd316597ae170d3bf8774f8f25cd99
Rimantas <belauzas@yahoo.com>
2021-05-25 19:19:50 +0300

    Tests for average function

--- js/fullname/fullname.test.js
+++ js/fullname/fullname.test.js
@@ -4,42 +4,42 @@ test('correct fullname', () => {
     expect(fullname('Vardenis', 'Pavardenis')).toBe('Vardenis Pavardenis');
 });
 
-test('error without firstname', () => {
-    expect(fullname('', 'Pavardenis')).toBe(false);
-});
+// test('error without firstname', () => {
+//     expect(fullname('', 'Pavardenis')).toBe(false);
+// });
 
-test('error without lastname', () => {
-    expect(fullname('Vardenis', '')).toBe(false);
-});
+// test('error without lastname', () => {
+//     expect(fullname('Vardenis', '')).toBe(false);
+// });
 
-test('fix firstname with spaces around', () => {
-    expect(fullname(' Vardenis', 'Pavardenis')).toBe('Vardenis Pavardenis');
-});
+// test('fix firstname with spaces around', () => {
+//     expect(fullname(' Vardenis', 'Pavardenis')).toBe('Vardenis Pavardenis');
+// });
 
-test('fix firstname with spaces around', () => {
-    expect(fullname('Vardenis ', 'Pavardenis')).toBe('Vardenis Pavardenis');
-});
+// test('fix firstname with spaces around', () => {
+//     expect(fullname('Vardenis ', 'Pavardenis')).toBe('Vardenis Pavardenis');
+// });
 
-test('fix firstname with spaces around', () => {
-    expect(fullname('     Vardenis', 'Pavardenis')).toBe('Vardenis Pavardenis');
-});
+// test('fix firstname with spaces around', () => {
+//     expect(fullname('     Vardenis', 'Pavardenis')).toBe('Vardenis Pavardenis');
+// });
 
-test('fix firstname with spaces around', () => {
-    expect(fullname('Vardenis     ', 'Pavardenis')).toBe('Vardenis Pavardenis');
-});
+// test('fix firstname with spaces around', () => {
+//     expect(fullname('Vardenis     ', 'Pavardenis')).toBe('Vardenis Pavardenis');
+// });
 
-test('fix lastname with spaces around', () => {
-    expect(fullname('Vardenis', ' Pavardenis')).toBe('Vardenis Pavardenis');
-});
+// test('fix lastname with spaces around', () => {
+//     expect(fullname('Vardenis', ' Pavardenis')).toBe('Vardenis Pavardenis');
+// });
 
-test('fix lastname with spaces around', () => {
-    expect(fullname('Vardenis', 'Pavardenis ')).toBe('Vardenis Pavardenis');
-});
+// test('fix lastname with spaces around', () => {
+//     expect(fullname('Vardenis', 'Pavardenis ')).toBe('Vardenis Pavardenis');
+// });
 
-test('fix lastname with spaces around', () => {
-    expect(fullname('Vardenis', '    Pavardenis')).toBe('Vardenis Pavardenis');
-});
+// test('fix lastname with spaces around', () => {
+//     expect(fullname('Vardenis', '    Pavardenis')).toBe('Vardenis Pavardenis');
+// });
 
-test('fix lastname with spaces around', () => {
-    expect(fullname('Vardenis', 'Pavardenis     ')).toBe('Vardenis Pavardenis');
-});
+// test('fix lastname with spaces around', () => {
+//     expect(fullname('Vardenis', 'Pavardenis     ')).toBe('Vardenis Pavardenis');
+// });
